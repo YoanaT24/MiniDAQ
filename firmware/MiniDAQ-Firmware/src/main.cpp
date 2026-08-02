617cd4f1e88717fcc45d11ac34c5c37f7d4ddd5d
YoanaT24 <ytroeva@tudelft.nl>
2026-08-02 23:59:57 +0200

    Verify ESP32 serial communication using USB cable

--- firmware/MiniDAQ-Firmware/src/main.cpp
+++ firmware/MiniDAQ-Firmware/src/main.cpp
@@ -1,25 +1,15 @@
 #include <Arduino.h>
 
-constexpr unsigned long SERIAL_BAUD_RATE = 115200;
-constexpr unsigned long MESSAGE_INTERVAL_MS = 1000;
-
-unsigned long previousMessageTime = 0;
-
 void setup() {
-    Serial.begin(SERIAL_BAUD_RATE);
+    Serial.begin(115200);
 
     delay(1000);
+
     Serial.println("MiniDAQ starting...");
 }
 
 void loop() {
-    const unsigned long currentTime = millis();
+    Serial.println("MiniDAQ is running");
 
-    if (currentTime - previousMessageTime >= MESSAGE_INTERVAL_MS) {
-        previousMessageTime = currentTime;
-
-        Serial.print("MiniDAQ uptime: ");
-        Serial.print(currentTime);
-        Serial.println(" ms");
-    }
+    delay(1000);
 }
